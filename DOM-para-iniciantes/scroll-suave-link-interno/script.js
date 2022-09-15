353f544069ca64e089cedc6cb243fa4fa2b1102b
Ana Carolina <anacaaa2709@gmail.com>
2022-09-14 22:41:05 -0300

    codigo aula de scroll suave

---
+++ DOM-para-iniciantes/scroll-suave-link-interno/script.js
@@ -0,0 +1,64 @@
+function initTabNav() { 
+  const tabMenu = document.querySelectorAll('.js-tabmenu li');
+  const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+  if(tabMenu.length && tabContent) { 
+    tabContent[0].classList.add('ativo');
+
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove('ativo');
+      })
+      tabContent[index].classList.add('ativo');
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', function() {
+        activeTab(index);
+      });
+    })
+  }
+}
+initTabNav();
+
+function initAccordion() { 
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const activeClass = 'ativo';
+  if(accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+    function activeAccordion(){ 
+      this.classList.add(activeClass);
+      this.nextElementSibling.classList.add(activeClass);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion);
+    })
+  }
+}
+initAccordion();
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    const topo = section.offsetTop;
+    window.scrollTo({
+    top: topo,
+    behavior: 'smooth',
+  });
+    // forma alternativa
+    // section.scrollIntoView({
+    //   behavior: 'smooth',
+    //   block: 'start',
+    // });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  })
+}
+initScrollSuave();
